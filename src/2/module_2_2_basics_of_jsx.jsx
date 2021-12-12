bdab0944500b3487d7e68c630f3824a63795bff3
Liran-tal <taliran44@gmail.com>
2021-12-12 15:07:18 +0200

    module_2_2_basics_of_jsx.jsx

--- src/2/module_2_2_basics_of_jsx.jsx
+++ src/2/module_2_2_basics_of_jsx.jsx
@@ -1,11 +1,20 @@
 function App() {
 	const data = ["hello", "world"];
+  const number1 = 5;
+  const number2 =6;
+  const string = "I love React!"
+  
   return (
     <div className="App">
       <p>
-				"{data[0]} {data[1]}"
+				1) "{data[0]} {data[1]}"
 			</p>
-			
+      <p>
+        2) {number1} + {number2} = {number1 + number2}
+      </p>
+      <p>
+        3) in "{string}" there are {string.length} characters
+      </p>
 			{/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
